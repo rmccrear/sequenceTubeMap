fa1706c0c792329faff497e44df22d1906c776c7
Robert Mccreary <fooyinfighter@gmail.com>
2023-02-26 23:15:39 -0500

    run serve

--- playwright.config.js
+++ playwright.config.js
@@ -86,7 +86,7 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run test-serve',
+    command: 'npm run serve',
     port: 3000,
     reuseExistingServer: !process.env.CI // use server if locally running
   },
